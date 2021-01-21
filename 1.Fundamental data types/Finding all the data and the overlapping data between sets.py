a4b2849048fe299ffe4836b9ddc76248daa7b27a
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-21 16:21:20 +0700

    Create Finding all the data and the overlapping data between sets.py

---
+++ 1.Fundamental data types/Finding all the data and the overlapping data between sets.py
@@ -0,0 +1,14 @@
+#Find the union: all_names
+all_names = baby_names_2011.union(baby_names_2014)
+
+# Print the count of names in all_names
+print(len(all_names))
+
+# Find the intersection: overlapping_names
+overlapping_names = baby_names_2011.intersection(baby_names_2014)
+
+# Print the count of names in overlapping_names
+print(len(overlapping_names))
+
+# Create the empty set: baby_names_2011
+baby_names_2011 = set()
